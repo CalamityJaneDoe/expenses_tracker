d8074be50c055e927ef5c3755cc3dd877680dc21
Calamity Jane <calamity_jane@macbook-air-de-calamity.home>
2023-05-03 20:36:58 +0200

    form inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,31 +1,20 @@
-import { useState } from 'react';
+import "./ExpenseItem.css";
 
-import './ExpenseItem.css';
-
-import Card from '../UI/Card';
-import ExpenseDate from './ExpenseDate';
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-
-    const [title, setTittle] = useState(props.title)
-
-
-    const clickHandler = () => {
-        setTittle('yolo')
-    }
-
-    return (
-        <Card className="expense-item">
-            <div>
-                <ExpenseDate date={props.date} />
-            </div>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>change title</button>
-        </Card>
-    );
+  return (
+    <Card className="expense-item">
+      <div>
+        <ExpenseDate date={props.date} />
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
 };
 
 export default ExpenseItem;
